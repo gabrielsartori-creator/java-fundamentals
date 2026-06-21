ce8226bc1a10d8ff40b1c470dc7d185b27bcdc0d
gabrielsartori-creator <gabrielmenegosssartori@gmail.com>
2026-06-21 12:09:22 -0300

    Create PrintfDemo.java

---
+++ 01.print-println-printf/PrintfDemo.java
@@ -0,0 +1,29 @@
+// Demonstration of System.out.printf() to Print a Person's Name and Age
+
+// Program Start
+
+// Main Class Declaration
+
+public class PrintfDemo
+{
+    // Main Method Declaration
+
+    public static void main(String[] args)
+    {
+        // Variable Declarations
+
+        String personName = "John Green"; // Stores a String value
+        int personAge = 25; // Stores an Integer value
+
+        // Call to the printf() Method
+
+        System.out.printf("%s is %d years old!", personName, personAge);
+    }
+
+    // End of Main Method
+
+}
+
+// End of Main Class
+
+// End of Program
