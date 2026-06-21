47523f574614052a6236a3fce12bc1c3a4bc53b6
gabrielsartori-creator <gabrielmenegosssartori@gmail.com>
2026-06-21 12:03:45 -0300

    Create PrintDemo.java

---
+++ 01.print-println-printf/PrintDemo.java
@@ -0,0 +1,29 @@
+// Demonstration of System.out.print() to Display Fruits on the Same Line
+
+// Program Start
+
+// Main Class Declaration
+
+public class PrintDemo
+{
+    // Main Method Declaration
+
+    public static void main(String[] args)
+    {
+        // Calls to the print() Method
+
+        System.out.print("Some examples of fruits are ");
+        System.out.print("Orange, ");
+        System.out.print("Watermelon, ");
+        System.out.print("Grapes, ");
+        System.out.print("Pineapple and ");
+        System.out.print("Strawberry.");
+    }
+
+    // End of Main Method
+
+}
+
+// End of Main Class
+
+// End of Program
