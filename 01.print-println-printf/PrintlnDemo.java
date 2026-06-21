d93ab4eb054f6f90e5df226f5ff5ec7d1685338a
gabrielsartori-creator <gabrielmenegosssartori@gmail.com>
2026-06-21 12:05:13 -0300

    Create PrintlnDemo.java

---
+++ 01.print-println-printf/PrintlnDemo.java
@@ -0,0 +1,37 @@
+// Demonstration of System.out.println() by Printing a Table
+// Containing the Squares of Numbers from 0 to 10
+
+// Program Start
+
+// Main Class Declaration
+
+public class PrintlnDemo
+{
+    // Main Method Declaration
+
+    public static void main(String[] args)
+    {
+        // Calls to the println() Method
+
+        System.out.println("Number | Square");
+        System.out.println("----------------");
+        System.out.println("  0    |      0");
+        System.out.println("  1    |      1");
+        System.out.println("  2    |      4");
+        System.out.println("  3    |      9");
+        System.out.println("  4    |     16");
+        System.out.println("  5    |     25");
+        System.out.println("  6    |     36");
+        System.out.println("  7    |     49");
+        System.out.println("  8    |     64");
+        System.out.println("  9    |     81");
+        System.out.println(" 10    |    100");
+    }
+
+    // End of Main Method
+
+}
+
+// End of Main Class
+
+// End of Program
